76fe78992db3a95a406ca7fff56229e07be5c81c
Jack Atherton <jackrrye@gmail.com>
2019-08-27 16:52:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,5 @@
 require_relative "../config/environment.rb"
-require 'active_support/inflector'
-require 'interactive_record.rb'
+
 
 class Student < InteractiveRecord
   self.column_names.each do |column_name|
